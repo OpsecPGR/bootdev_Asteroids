15ff5c7bf0adcd45f7924d3cdaa5f070f3b0136a
OpsecPGR <OpsecPGR@protonmail.com>
2025-09-23 14:11:24 -0500

    Main window can be opened and closed.

---
+++ main.py
@@ -0,0 +1,21 @@
+from constants import *
+import pygame
+
+def main():
+    pygame.init()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    should_exit = False
+    while not(should_exit):
+        for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                            return
+        screen.fill("black")
+        pygame.display.flip()
+        
+
+
+if __name__ == "__main__":
+    main()
