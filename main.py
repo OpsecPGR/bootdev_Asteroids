18bae40e9395337baf2e74a4f929f42079f20b0c
OpsecPGR <OpsecPGR@protonmail.com>
2025-09-23 14:29:33 -0500

    Added framerate fixing

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pygame
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -14,7 +16,8 @@ def main():
                             return
         screen.fill("black")
         pygame.display.flip()
-        
+        dt = clock.tick(60)
+
 
 
 if __name__ == "__main__":
